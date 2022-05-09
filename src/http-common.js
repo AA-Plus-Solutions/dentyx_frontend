ab250eeb7856eacd92d3a603d3671989af994fd5
Oscar Rosete <oscaralonso11@hotmail.com>
2022-05-09 00:32:34 -0700

    feat: added backend production url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api/v1/reviews",
+    baseURL: "https://dentyx-reviews.herokuapp.com/api/v1/reviews",
     headers: {
         "Content-type": "application/json"
     }
